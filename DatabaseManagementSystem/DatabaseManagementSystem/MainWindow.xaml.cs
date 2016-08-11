f7fb92d77e4663ce1c6059d9ef722bab1e8c7e4b
steve emman <thevoice_7@hotmail.com>
2016-08-11 18:23:11 +0300

    First version of GUI

--- DatabaseManagementSystem/DatabaseManagementSystem/MainWindow.xaml.cs
+++ DatabaseManagementSystem/DatabaseManagementSystem/MainWindow.xaml.cs
@@ -15,19 +15,74 @@ using System.Windows.Shapes;
 
 namespace DatabaseManagementSystem
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        MySql.Data.MySqlClient.MySqlConnection conn;
+
         public MainWindow()
         {
             InitializeComponent();
+            ConnectToDatabase();
+            SetStation("No station selected");
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        public void ConnectToDatabase()
         {
-            Console.WriteLine("Hello m8");
+            string myConnectionString;
+            
+            myConnectionString = "server=127.0.0.1;uid=root;" +
+                "pwd=;database=test2;";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+                DatabaseManagementSystem.NotificationWindow nw = new NotificationWindow("Connection successful!");
+                nw.Show();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                DatabaseManagementSystem.NotificationWindow nw = new NotificationWindow("Connection failed!");
+                nw.Show();
+            }
+        }
+
+        private void SetStation(string v)
+        {
+            SelectedStation.Text = "Station | " + v;
+        }
+
+        private void SetConnection
+
+        private void EnableMenu()
+        {
+            MenuItem_Insert.IsEnabled = true;
+            MenuItem_View.IsEnabled = true;
+        }
+
+        private void MenuItem_Insert_Click(object sender, RoutedEventArgs e)
+        {
+            DatabaseManagementSystem.DatabaseEntry de = new DatabaseEntry();
+            de.Show();
+        }
+
+        private void MenuItem_Laser_Click(object sender, RoutedEventArgs e)
+        {
+            EnableMenu();
+            SetStation("Laser");
+        }
+
+        private void MenuItem_3DPrinter_Click(object sender, RoutedEventArgs e)
+        {
+            EnableMenu();
+            SetStation("3DPrinter");
+        }
+
+        private void MenuItem_Retry_Click(object sender, RoutedEventArgs e)
+        {
+            ConnectToDatabase();
         }
     }
 }
